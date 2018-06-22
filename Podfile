2bbb5d9bff8ce8ef25e9c64c48ac478427d93636
juyw <757832801@qq.com>
2018-06-22 18:44:34 +0800

    init

---
+++ Podfile
@@ -0,0 +1,14 @@
+
+
+
+platform:ios,'9.0'
+
+target 'test' do
+
+
+
+pod 'AFNetworking', '~> 3.1.0'
+
+
+end
+
---
+++ test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by 居祎炜 on 2018/4/23.
+//  Copyright © 2018年 居祎炜. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *testLabel;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+self.testLabel.text = @"fk - 飞科";
+
+}
+
+
+@end
