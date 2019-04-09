9b5fa927869c418ac0a1dc7d11517cc46366a175
juyw <757832801@qq.com>
2019-04-09 15:45:35 +0800

    test

---
+++ test/TestView.h
@@ -0,0 +1,17 @@
+//
+//  TestView.h
+//  test
+//
+//  Created by juyiwei on 2019/4/9.
+//  Copyright © 2019 居祎炜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ test/TestView.m
@@ -0,0 +1,21 @@
+//
+//  TestView.m
+//  test
+//
+//  Created by juyiwei on 2019/4/9.
+//  Copyright © 2019 居祎炜. All rights reserved.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
